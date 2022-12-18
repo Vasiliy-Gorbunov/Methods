fa0557b99df91f61272e345b16966c8d556c724c
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-18 22:02:20 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        enterCurrentYear();
     }
 
-    public static int enterCurrentYear() {
+    public static void enterCurrentYear() {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите год: ");
-        int currentYear = in.nextInt();
-        return currentYear;
+        int yearNow = in.nextInt();
+        task1(yearNow);
     }
 
-    public static void task1() {
-        int currentYear = enterCurrentYear();
+    public static void task1(int currentYear) {
         if (currentYear % 4 == 0 && currentYear % 100 != 0 || currentYear % 400 == 0) {
             System.out.printf("%d год - високосный!", currentYear);
         } else System.out.printf("%d год - не високосный!", currentYear);
