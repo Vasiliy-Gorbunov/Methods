6fc6babed0e19b28007b7842789465cbc6f6acd4
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-18 22:37:22 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        enterCurrentYear();
+        //enterCurrentYear();
+        task2();
     }
 
     public static void enterCurrentYear() {
@@ -17,4 +19,31 @@ public class Main {
             System.out.printf("%d год - високосный!", currentYear);
         } else System.out.printf("%d год - не високосный!", currentYear);
     }
+
+    public static int enterClientOS() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("\nВаша ОС (0 - iOS, 1 - Android): ");
+        int clientOS = in.nextInt();
+        return clientOS;
+    }
+
+    public static int getClientDeviceYear() {
+        int currentYear = LocalDate.now().getYear();
+        return currentYear;
+    }
+
+    public static void task2() {
+        if (getClientDeviceYear() >= 2015) {
+            if (enterClientOS() == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (enterClientOS() == 0) {
+            System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+        }
+    }
+
 }
